44ef08c265499cfb7320a6797af9fef10015207e
CoderFrede1010 <frfu0001@stud.kea.dk>
2024-10-21 11:23:52 +0200

    //komentar

--- src/Salon_BookningsSystem.java
+++ src/Salon_BookningsSystem.java
@@ -1,5 +1,5 @@
 public class Salon_BookningsSystem {
-    //konsolbaseret single-user bookingsystem. push test
+    //konsolbaseret single-user bookingsystem. push test//l
     public static void main(String[] args) {
     }
 }
