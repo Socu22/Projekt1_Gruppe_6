8650a5ad06193a71e41ccc09e0765840907b2525
LucasPK <149070654+Socu22@users.noreply.github.com>
2024-10-21 11:31:14 +0200

    slut push test

--- src/Salon_BookningsSystem.java
+++ src/Salon_BookningsSystem.java
@@ -1,6 +1,7 @@
 public class Salon_BookningsSystem {
-    //konsolbaseret single-user bookingsystem. mit push
+    //konsolbaseret single-user bookingsystem.
     public static void main(String[] args) {
-//test1
+
     }
 }
+
