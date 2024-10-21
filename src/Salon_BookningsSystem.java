3d976c6434e0020d35dfdc5e5e7419bbe0b38231
KEAmimi <mimi0001@stud.kea.dk>
2024-10-21 22:04:05 +0200

    Git virker

--- src/Salon_BookningsSystem.java
+++ src/Salon_BookningsSystem.java
@@ -1,7 +1,7 @@
 public class Salon_BookningsSystem {
     //konsolbaseret single-user bookingsystem.
     public static void main(String[] args) {
-
+        System.out.println("Hej Drenge, jeg er glad for at rapportere, at mit Git virker igen!");
     }
 }
 
