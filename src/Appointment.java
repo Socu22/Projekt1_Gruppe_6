3bdaafe5ddaca201a295b7c1d301c42e3c01f2cf
LucasPK <149070654+Socu22@users.noreply.github.com>
2024-10-22 16:03:25 +0200

    BookingHandler updates

--- src/Appointment.java
+++ src/Appointment.java
@@ -1,2 +1,14 @@
 public class Appointment {
+
+    int id=0;
+
+    Appointment( ){
+        id=id+1;
+    }
+
+
+    @Override
+    public String toString() {
+        return ""+id;
+    }
 }
--- src/BookingHandler.java
+++ src/BookingHandler.java
@@ -1,20 +1,58 @@
+import java.util.Collection;
+import java.util.Collections;
+
 public class BookingHandler extends MenuAndInterface {
     public static void main(String[] args) {
+        BookingHandler b = new BookingHandler(); //test
+
+        //b.createBooking(2); //create in a specific timestamp
+        b.sendTSIDArrayToArrayList(); //send to arraylist (in Menu and Inteface) to send in the timestamps so it can be changed in a specific way.
+        //System.out.println(appointmentsArrayList.size()); //confirms that arraylist has increased
+        //System.out.println(appointmentsArrayList.get(2)); //confirms that appointment is there (create a booking)
+        b.createBookingToSpecificDay(1,1);
+
+        b.sendTSIDArrayToArrayList();
+        b.createBookingToSpecificDay(2,1);
+
+
+        System.out.println(appointmentsArrayList.get(0)); //confirms that appointment is there (addAppointmentTOADay)
+
+        System.out.println("test");
+        for (Appointment a : appointmentsArrayList){
+            System.out.println(a);
+
+        }
+
+
+
+
+
 
     }
 
-    static Appointment[] timeSlotInADay= new Appointment[8];
+    final static Appointment[] timeSlotInADay= new Appointment[8];
     static int bookingNr = 0;
 
-
-    public void createBooking(){
-
-        timeSlotInADay[bookingNr]= new Appointment();
-        bookingNr++;
+    public void sendTSIDArrayToArrayList(){
+        Collections.addAll(appointmentsArrayList,timeSlotInADay);
 
     }
-    public void deleteBooking(int bookingNr){
-        timeSlotInADay[bookingNr]==new Appointment().n;
+    public void createBookingToSpecificDay(int dayNo, int posOneToEight) {
+        int index = (dayNo - 1) * 8 + (posOneToEight - 1);  // Calculate the correct index
+        if (index < appointmentsArrayList.size()) {
+            appointmentsArrayList.set(index, new Appointment());  // Replace existing appointment
+        } else {
+            System.out.println("Error: Invalid booking index.");
+        }
+    }
+    public void createBooking(int pos){
+
+        //timeSlotInADay[pos]= new Appointment();
+
+
+    }
+    public void deleteBooking(int delPos){
+        //timeSlotInADay[bookingNr]==new Appointment();
 
     }
     public void showBooking(){}
