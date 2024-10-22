624fd986d589f3aa5cb6bce9f244e7e302c3b45c
LucasPK <149070654+Socu22@users.noreply.github.com>
2024-10-22 11:49:44 +0200

    set new classes

--- src/MenuAndInterface.java
+++ src/MenuAndInterface.java
@@ -1,4 +1,4 @@
-public class BookningSoftware {
+public class MenuAndInterface {
     //konsolbaseret single-user bookingsystem.
     public static void main(String[] args) {
         System.out.println("Hej Drenge, jeg er glad for at rapportere, at mit Git virker igen!");
