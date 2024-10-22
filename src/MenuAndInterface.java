daf53426493666677cad5535ad37a021068c0112
LucasPK <149070654+Socu22@users.noreply.github.com>
2024-10-22 12:04:11 +0200

    Test Arraylist 365 dage

--- src/MenuAndInterface.java
+++ src/MenuAndInterface.java
@@ -1,7 +1,19 @@
+import java.util.ArrayList;
+
 public class MenuAndInterface {
     //konsolbaseret single-user bookingsystem.
+
+    static protected ArrayList<Appointment> appointmentsArrayList = new ArrayList<>(365);//Forsøg på at skabe 365 dage
     public static void main(String[] args) {
         System.out.println("Hej Drenge, jeg er glad for at rapportere, at mit Git virker igen!");
+
+        for (int i = 0; i < 365 ; i++) { //skaber det
+            appointmentsArrayList.add(null);
+
+        }
+        System.out.println(appointmentsArrayList.size()); // tjekker at arraylist har den korekte size
+
+
     }
 
 }
