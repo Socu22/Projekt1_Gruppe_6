6fcd5f8d52759dc3a260ee34e4efa692a81da76a
LucasPK <149070654+Socu22@users.noreply.github.com>
2024-10-22 16:41:42 +0200

    BookingHandler updates

--- src/BookingHandler.java
+++ src/BookingHandler.java
@@ -1,27 +1,33 @@
-import java.util.Collection;
 import java.util.Collections;
 
 public class BookingHandler extends MenuAndInterface {
     public static void main(String[] args) {
         BookingHandler b = new BookingHandler(); //test
+        int i =1;
+        while (true) {
+            //b.createBooking(2); //create in a specific timestamp
+            b.nextDay(); //send to arraylist (in Menu and Inteface) to send in the timestamps so it can be changed in a specific way.
+            //System.out.println(appointmentsArrayList.size()); //confirms that arraylist has increased
+            //System.out.println(appointmentsArrayList.get(2)); //confirms that appointment is there (create a booking)
+            b.createBookingToSpecificDay(i, 1);
+            i++;
 
-        //b.createBooking(2); //create in a specific timestamp
-        b.sendTSIDArrayToArrayList(); //send to arraylist (in Menu and Inteface) to send in the timestamps so it can be changed in a specific way.
-        //System.out.println(appointmentsArrayList.size()); //confirms that arraylist has increased
-        //System.out.println(appointmentsArrayList.get(2)); //confirms that appointment is there (create a booking)
-        b.createBookingToSpecificDay(1,1);
-
-        b.sendTSIDArrayToArrayList();
-        b.createBookingToSpecificDay(2,1);
+            b.nextDay();
+            b.createBookingToSpecificDay(2, 1);
+            b.createBookingToSpecificDay(2, 1);
 
 
-        System.out.println(appointmentsArrayList.get(0)); //confirms that appointment is there (addAppointmentTOADay)
+            // System.out.println(appointmentsArrayList.get(0)); //confirms that appointment is there (createBookingToSpecificDay)
 
-        System.out.println("test");
-        for (Appointment a : appointmentsArrayList){
-            System.out.println(a);
+            // b.deleteBookingToSpecificDay(1,1);
+            //System.out.println(appointmentsArrayList.get(0)); //confirms that appointment is deleted (deleteBookingToSpecificDay)
 
+
+            b.showBooking();
         }
+
+
+
 
 
 
@@ -33,7 +39,7 @@ public class BookingHandler extends MenuAndInterface {
     final static Appointment[] timeSlotInADay= new Appointment[8];
     static int bookingNr = 0;
 
-    public void sendTSIDArrayToArrayList(){
+    public void nextDay(){
         Collections.addAll(appointmentsArrayList,timeSlotInADay);
 
     }
@@ -51,10 +57,19 @@ public class BookingHandler extends MenuAndInterface {
 
 
     }
-    public void deleteBooking(int delPos){
-        //timeSlotInADay[bookingNr]==new Appointment();
-
+    public void deleteBookingToSpecificDay(int dayNo, int posOneToEight) {
+        int index = (dayNo - 1) * 8 + (posOneToEight - 1);  // Calculate the correct index
+        if (index < appointmentsArrayList.size()) {
+            appointmentsArrayList.set(index, null);  // Replace existing appointment
+        } else {
+            System.out.println("Error: Invalid booking index.");
+        }
     }
-    public void showBooking(){}
+    public void showBooking(){
+        for (int i = 0; i < appointmentsArrayList.size(); i++) {
+            System.out.println(appointmentsArrayList.get(i));
+
+        }
+    }
     public void selectBooking(){}
 }
