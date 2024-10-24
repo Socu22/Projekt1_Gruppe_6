5f929a6e66b6a1e088371cfb07c1c39f93620b41
CoderFrede1010 <frfu0001@stud.kea.dk>
2024-10-24 02:34:25 +0200

    // opdatering på EconomyLogHandler

--- src/EconomyLogHandler.java
+++ src/EconomyLogHandler.java
@@ -1,2 +1,15 @@
 public class EconomyLogHandler {
+    public String seeEarnings(date) { // metode som printer regninger for en angiven dato
+
+        System.out.println("Fakturene for dagen :" + date);
+        i = timeSlot;
+    for (int i = 0; i < 8; i++) {
+        System.out.println("Kunden " + name + "har betalt"+price);
+
+    }
+}
+
+public int showCredit(date){ // metode som viser credit på angiven dato
+    System.out.println("For bookingen "+bookingId+ "har "+name+" kredit på : "+ credit);
+}
 }
