a455660a33e2be1ecbbf004ff90082303f9c0f9c
CoderFrede1010 <frfu0001@stud.kea.dk>
2024-10-24 12:13:29 +0200

    // opdatering på EconomyLogHandler

--- src/EconomyLogHandler.java
+++ src/EconomyLogHandler.java
@@ -1,15 +1,53 @@
-public class EconomyLogHandler {
-    /*public String seeEarnings(date) { // metode som printer regninger for en angiven dato
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
-        System.out.println("Fakturene for dagen :" + date);
-        i = timeSlot;
-    for (int i = 0; i < 8; i++) {
-        System.out.println("Kunden " + name + "har betalt"+price);
+public class EconomyLogHandler {
+
+   /* private FileHandler fileHandler;
+
+
+    public void seeEarnings() { // metode som printer regninger for en angiven dato
+
+        System.out.println("Skriv en dato  ÅR-MÅNED-DAG");
 
     }
-}
 
-public int showCredit(date){ // metode som viser credit på angiven dato
-    System.out.println("For bookingen "+bookingId+ "har "+name+" kredit på : "+ credit);
-} */
+    public String showCredit() { // metode som viser credit på
+        Scanner tastBookingId = new Scanner(System.in);
+
+            ArrayList<Appointment> appointments = fileHandler.getList();
+            for (Appointment appointment : appointments) {
+                if (appointment.getBookingCredit() == appointmentCredit) {
+                    return true; // Gyldig appointmentId
+                }
+            }
+        }
+
+    public void startEconomyMenu() {
+        Scanner economyMenuChoice = new Scanner(System.in);
+        boolean out = false;
+
+        while (!out) {
+            System.out.println("Tast 1 for at printe fakturer for en angiven dag");
+            System.out.println("Tast 2 for at vise kredit for et angiven bookingID");
+            System.out.println("Tast 3 for at gå tilbage til hovedmenu");
+
+            int economyChoice = economyMenuChoice.nextInt();
+
+            switch (economyChoice) {
+
+                case 1:
+                    seeEarnings();
+                    break;
+                case 2: showCredit();
+                    break;
+                case 3:
+                    System.out.println("");
+                    out = true;
+                    break;
+            }
+
+        }
+    }*/
 }
