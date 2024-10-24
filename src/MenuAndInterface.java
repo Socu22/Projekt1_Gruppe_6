aff2f1f92d8dba017ab492e0980cd365ec62ca52
LucasPK <149070654+Socu22@users.noreply.github.com>
2024-10-24 10:36:24 +0200

    BookingJHanlder og Ny arraylist

--- src/MenuAndInterface.java
+++ src/MenuAndInterface.java
@@ -4,6 +4,7 @@ public class MenuAndInterface {
     //konsolbaseret single-user bookingsystem.
 
     static protected ArrayList<Appointment2> appointmentsArrayList = new ArrayList<>(365);//Bruges i Bookinghandler
+    static protected ArrayList<Appointment> DontFuckTHisappointmentsArrayList = new ArrayList<>(365);//Bruges i Bookinghandler
     public static void main(String[] args) {
 
         for (int i = 0; i < 365 ; i++) { //skaber det
