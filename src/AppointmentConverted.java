6e9dbe1b09a8cb1743d754e9453f0209462e2894
oahlers <oliv2527@stud.kea.dk>
2024-10-25 10:55:08 +0200

    SALONSYSTEM VOL 7 + registercredit updated

--- src/AppointmentConverted.java
+++ src/AppointmentConverted.java
@@ -27,6 +27,7 @@ public class AppointmentConverted {
         this.phoneNumber = appointmentForConvert.getPhoneNumber();
         this.price = appointmentForConvert.getPrice();
         this.isBooked = false;
+        this.credit = appointmentForConvert.getCredit();
     }
 
     public static void setAccountCounter(int accountCounter) {
