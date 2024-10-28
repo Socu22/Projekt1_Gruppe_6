189a9b3609d69be00373f7aa0ca3f392df7962c3
KEAmimi <mimi0001@stud.kea.dk>
2024-10-28 11:51:35 +0100

    Appointment Converted

--- src/FileHandler.java
+++ src/FileHandler.java
@@ -12,6 +12,7 @@ import com.google.gson.reflect.TypeToken;
 public class FileHandler {
     //The arraylist of appointments in memory
     private ArrayList<Appointment> listOfAppointments = new ArrayList<>();
+    //The arraylist of converted appopintments
     private ArrayList<AppointmentConverted> convertedAppointments = new ArrayList<>();
 
     //Creates a Gsonbuilder to convert variables into Json. Will be used to convert the arraylist of appointments to and from Json.
@@ -36,7 +37,7 @@ public class FileHandler {
     void saveCalendar() throws IOException {
         Appointment.accountCounter = 1;
         AppointmentConverted.accountCounter = 1;
-        FileWriter jsonWriter = new FileWriter("src//TestFile6.json", false);
+        FileWriter jsonWriter = new FileWriter("src//Calendar2024-2025.json", false);
         convertToStrings(listOfAppointments);
         String jsonToWrite;
         jsonWriter.write("[");
@@ -56,7 +57,7 @@ public class FileHandler {
     ArrayList loadCalendar() throws FileNotFoundException {
         Appointment.accountCounter = 1;
         AppointmentConverted.accountCounter = 1;
-        FileReader jsonReader = new FileReader("src//TestFile6.json");
+        FileReader jsonReader = new FileReader("src//Calendar2024-2025.json");
         JsonElement jsonText = gson.fromJson(jsonReader, JsonElement.class);
 
         Type appointmentListType = new TypeToken<ArrayList<AppointmentConverted>>() {}.getType();
@@ -88,145 +89,10 @@ public class FileHandler {
 
 
     public static void main(String[] args) throws Exception {
-
         FileHandler f1 = new FileHandler();
-        BookingHandler b=new BookingHandler();
-        b.findAppointment_WithId(1,f1).setName("Mikkel");
+        BookingHandler b = new BookingHandler(f1);
+        b.findAppointment_WithId(1).setName("Mikkel");
         f1.saveCalendar();
         f1.saveCalendar();
-
-        /*
-        //This is an imaginary appointment coming from the software
-        TestObject t1 = new TestObject("t1", 1, LocalDate.of(2024,01,01));
-        System.out.println(t1);
-
-        //Convert the appointment into values that json can handle (date and time to strings)
-        TestObjectForSerialization ts1 = new TestObjectForSerialization(t1);
-        System.out.println(ts1);
-
-        //Writes the object to a json file
-        String jsonToWrite = gson.toJson(ts1);
-        FileWriter jsonWriter = new FileWriter("TestFile.json", false);
-        jsonWriter.write(jsonToWrite);
-        jsonWriter.close();
-        System.out.println("File created and saved");
-
-        //Read the json file
-        FileReader jsonReader = new FileReader("TestFile.json");
-        JsonElement jsonText = gson.fromJson(jsonReader, JsonElement.class);
-        System.out.println(jsonText);
-
-        //Convert the json file back into the object
-        TestObjectForSerialization ts2 = (gson.fromJson(jsonText, TestObjectForSerialization.class));
-        System.out.println(ts2);
-        t1 = new TestObject(ts2);
-        System.out.println(t1);
-         */
-
-
-
-        /*
-        //Test arraylist of objects.
-        ArrayList<TestObject> listOfTestObjects = new ArrayList<>();
-
-        for (int i = 1; i<=10; i++){
-            TestObject t = new TestObject("t1", i, LocalDate.of(2024,01,i));
-            listOfTestObjects.add(t);
-        }
-        System.out.println(listOfTestObjects);
-
-        ArrayList<TestObjectForSerialization> convertedlist = new ArrayList<>();
-        for (TestObject obj: listOfTestObjects){
-            TestObjectForSerialization ts = new TestObjectForSerialization(obj);
-            convertedlist.add(ts);
-        }
-
-        System.out.println(convertedlist);
-
-        String jsonToWrite = gson.toJson(convertedlist);
-        FileWriter jsonWriter = new FileWriter("TestFile.json", false);
-        jsonWriter.write(jsonToWrite);
-        jsonWriter.close();
-        System.out.println("File created and saved");
-
-        FileReader jsonReader = new FileReader("TestFile.json");
-        JsonElement jsonText = gson.fromJson(jsonReader, JsonElement.class);
-        System.out.println(jsonText);
-
-        Type testObjectListType = new TypeToken<ArrayList<TestObjectForSerialization>>() {}.getType();
-        ArrayList<TestObjectForSerialization> deserializedList = gson.fromJson(jsonText, testObjectListType);
-        System.out.println(deserializedList);
-
-        for (TestObjectForSerialization object: deserializedList){
-            TestObject t = new TestObject(object);
-            System.out.println(t);
-        }
-
-         */
-    }
-    //Booking File has to be created
-
-}
-
-class TestObjectForSerialization{
-    private String convertedDate;
-    private int testInt;
-    private String testString;
-
-    TestObjectForSerialization(TestObject testObject){
-        this.convertedDate = testObject.dateGetter().toString();
-        this.testString = testObject.stringGetter();
-        this.testInt = testObject.numbergetter();
-    }
-
-    String dateGetter(){
-        return convertedDate;
-    }
-
-    String stringGetter(){
-        return testString;
-    }
-
-    int numberGetter(){
-        return testInt;
-    }
-
-    public String toString(){
-        return "CONVERTED: test: " + testString + " number: " + testInt + " date: " + convertedDate;
-    }
-}
-
-
-class TestObject{
-    private LocalDate date;
-    private int testInt;
-    private String testString;
-
-    TestObject (String string, int number, LocalDate date){
-        this.date = date;
-        this.testString = string;
-        this.testInt = number;
-    }
-
-    TestObject (TestObjectForSerialization testObjectForDeserialization){
-        this.date = LocalDate.parse(testObjectForDeserialization.dateGetter());
-        this.testString = testObjectForDeserialization.stringGetter();
-        this.testInt = testObjectForDeserialization.numberGetter();
-    }
-
-    LocalDate dateGetter(){
-        return date;
-    }
-
-    String stringGetter(){
-        return testString;
-    }
-
-    int numbergetter(){
-        return testInt;
-    }
-
-    public String toString(){
-        return "test: " + testString + " number: " + testInt + " date: " + date;
     }
 }
